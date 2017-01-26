82c57c837f71f3a855f9dd2d60bc0499507d2a10
Casey Clayton <claytoncasey01@gmail.com>
2017-01-25 23:03:07 -0600

    Added creature sizing and movment

--- src/com/bearcavestudios/tilerpg/Game.java
+++ src/com/bearcavestudios/tilerpg/Game.java
@@ -68,6 +68,13 @@ public class Game implements Runnable {
 		// Clear Screen
 		g.clearRect(0, 0, width, height);
 		
+		// Test code to render grass
+		for(int x = 0; x <= width; x++) {
+			for(int y = 0; y < height; y++) {
+				g.drawImage(Assets.grass, x * 32, y * 32, null);
+			}
+		}
+		
 		if(State.getState() != null) {
 			State.getState().render(g);
 		}
