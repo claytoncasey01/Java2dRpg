0bffa3a7e539d0260f061acc4b38214c6314a8b8
Casey Clayton <claytoncasey01@gmail.com>
2017-01-28 17:57:33 -0600

    Added Animation class and character spritesheet

--- src/com/bearcavestudios/tilerpg/entities/creatures/Player.java
+++ src/com/bearcavestudios/tilerpg/entities/creatures/Player.java
@@ -2,25 +2,46 @@ package com.bearcavestudios.tilerpg.entities.creatures;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import com.bearcavestudios.tilerpg.Handler;
+import com.bearcavestudios.tilerpg.gfx.Animation;
 import com.bearcavestudios.tilerpg.gfx.Assets;
 
 public class Player extends Creature {
+	
+	// Animations
+	private Animation animIdle, animUp, animDown, animLeft, animRight;
 	
 
 	public Player(Handler handler, float x, float y) {
 		super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
 		
 		// Setup bounds for collision
-		bounds.x = 6;
-		bounds.y = 4;
-		bounds.width = 24;
+		bounds.x = 10;
+		bounds.y = 8;
+		bounds.width = 12;
 		bounds.height = 24;
+		
+		// Animations
+		animIdle = new Animation(0, Assets.player_idle);
+		animUp = new Animation(100, Assets.player_up);
+		animDown = new Animation(100, Assets.player_down);
+		animLeft = new Animation(100, Assets.player_left);
+		animRight = new Animation(100, Assets.player_right);
+		
 	}
 
 	@Override
 	public void tick() {
+		// Animations
+		animIdle.tick();
+		animUp.tick();
+		animDown.tick();
+		animLeft.tick();
+		animRight.tick();
+		
+		// Movement
 		getInput();
 		move();
 		
@@ -30,7 +51,7 @@ public class Player extends Creature {
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.player, (int)(x - handler.getCamera().getxOffset()), 
+		g.drawImage(getCurrentAnimationFrame(), (int)(x - handler.getCamera().getxOffset()), 
 				(int)(y - handler.getCamera().getyOffset()), width, height, null);
 		
 		// Test code to show bounding box for collision detection
@@ -38,6 +59,24 @@ public class Player extends Creature {
 		g.fillRect((int)(x + bounds.x - handler.getCamera().getxOffset()), 
 				(int)(y + bounds.y - handler.getCamera().getyOffset()), bounds.width, bounds.height);*/
 		
+	}
+	
+	private BufferedImage getCurrentAnimationFrame() {
+		BufferedImage frame;
+		
+		if(xMove < 0) {
+			frame = animLeft.getCurrentFrame();
+		} else if(xMove > 0) {
+			frame = animRight.getCurrentFrame();
+		} else if(yMove < 0) {
+			frame = animUp.getCurrentFrame();
+		} else if(yMove > 0) {
+			frame = animDown.getCurrentFrame();
+		} else {
+			frame = animIdle.getCurrentFrame();
+		}
+		
+		return frame;
 	}
 	
 	private void getInput() {
--- src/com/bearcavestudios/tilerpg/gfx/Assets.java
+++ src/com/bearcavestudios/tilerpg/gfx/Assets.java
@@ -5,15 +5,34 @@ import java.awt.image.BufferedImage;
 public class Assets {
 	
 	private static final int WIDTH = 32, HEIGHT = 32;
-	public static BufferedImage grass, rock, dirt, player;
+	private static final int PLAYER_WIDTH = 64, PLAYER_HEIGHT = 64;
+	public static BufferedImage grass, rock, dirt;
+	public static BufferedImage[] player_idle, player_up, player_down, player_left, player_right;
 	
 
 	public static void init() {
 		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/forest.png"));
+		SpriteSheet playerSheet = new SpriteSheet(ImageLoader.loadImage("/textures/character.png"));
 		grass = sheet.crop(0, 0, WIDTH, HEIGHT);
 		rock = sheet.crop(32 * 1, 0, WIDTH, HEIGHT);
 		dirt = sheet.crop(32 * 2, 0, WIDTH, HEIGHT);
-		// change to a different sprite later on
-		player = sheet.crop(0, 64, WIDTH, HEIGHT);
+		
+		// Player down animations
+		player_idle = new BufferedImage[1];
+		player_up = new BufferedImage[9];
+		player_down = new BufferedImage[9];
+		player_left = new BufferedImage[9];
+		player_right = new BufferedImage[9];
+		
+		player_idle[0] = playerSheet.crop(0, 2 * PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT);
+		
+		// Handle player animation sprites.
+		for(int i = 0; i < player_down.length; i++) {
+			player_up[i] = playerSheet.crop(i * PLAYER_WIDTH, 8 * PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT);
+			player_down[i] = playerSheet.crop(i * PLAYER_WIDTH, 10 * PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT);
+			player_left[i] = playerSheet.crop(i * PLAYER_WIDTH, 9 * PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT);
+			player_right[i] = playerSheet.crop(i * PLAYER_WIDTH, 11 * PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT);
+		}
+
 	}
 }
