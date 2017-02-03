7080dfc79b5380031a49f53daa1471d08aba8909
Casey Clayton <claytoncasey01@gmail.com>
2017-02-02 23:24:22 -0600

    Updates for proper render order

--- src/com/bearcavestudios/tilerpg/entities/EntityManager.java
+++ src/com/bearcavestudios/tilerpg/entities/EntityManager.java
@@ -2,6 +2,7 @@ package com.bearcavestudios.tilerpg.entities;
 
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import com.bearcavestudios.tilerpg.Handler;
 import com.bearcavestudios.tilerpg.entities.creatures.Player;
@@ -12,6 +13,18 @@ public class EntityManager {
 	private Handler handler;
 	private Player player;
 	private ArrayList<Entity> entities;
+	private Comparator<Entity> renderSorter = new Comparator<Entity>() {
+
+		@Override
+		public int compare(Entity a, Entity b) {
+			if(a.getY() + a.getHeight() < b.getY() + b.getHeight()) {
+				return -1;
+			} else {
+				return 1;
+			}
+		}
+		
+	};
 	
 	public EntityManager(Handler handler, Player player) {
 		this.handler = handler;
@@ -26,6 +39,7 @@ public class EntityManager {
 			Entity e = entities.get(i);
 			e.tick();
 		}
+		entities.sort(renderSorter);
 	}
 	
 	public void render(Graphics g) {
